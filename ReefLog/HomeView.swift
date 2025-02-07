ac1389499711ab58ffc8f19a4e58cb22d2a85511
alhwyn <alhwyndwyne0411@gmail.com>
2025-02-06 21:43:44 -0800

    Initial Commit

---
+++ ReefLog/HomeView.swift
@@ -0,0 +1,34 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        VStack {
+            Text("All About")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+
+            Image(information.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(10)
+                .padding(40)
+
+            Text(information.name)
+                .font(.title)
+        }
+    }
+    
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
+
---
+++ ReefLogTests/ReefLogTests.swift
@@ -0,0 +1,16 @@
+//
+//  ReefLogTests.swift
+//  ReefLogTests
+//
+//  Created by Alhwyn Geonzon on 2025-02-06.
+//
+
+import Testing
+
+struct ReefLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
